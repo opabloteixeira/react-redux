8f0d12653fdd740eb8c0bedf7a3ea5658d1e36be
Pablo Teixeira <pablopst@gmail.com>
2020-10-20 19:43:02 -0300

    check product

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
-import { addProductToCart } from '../store/modules/cart/actions';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
 
 
@@ -15,7 +15,7 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
 
    const handleAddProductToCart = useCallback(() => {
 
-      dispatch(addProductToCart(product));
+      dispatch(addProductToCartRequest(product));
 
    },[dispatch, product]);
 
--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,8 +1,17 @@
-import { all, select, takeLatest } from 'redux-saga/effects';
-import { IState } from '../..';
-import { addProductToCart } from './actions';
+import { AxiosResponse } from 'axios';
+import { all, select, takeLatest, call } from 'redux-saga/effects';
 
-type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+import { IState } from '../..';
+import { addProductToCartRequest } from './actions';
+import api from '../../../services/api';
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
+
+interface IStockResponse {
+   id: number;
+   quantity: number;
+}
+
 
 function* checkProductStock({ payload }: CheckProductStockRequest){
    const { product } = payload;
@@ -11,10 +20,17 @@ function* checkProductStock({ payload }: CheckProductStockRequest){
       return state.cart.items.find( item => item.product.id === product.id)?.quantity ?? 0;
    })
 
-   console.log(currentQuantity);
+   const avalableStockResponse: AxiosResponse<IStockResponse> = yield call(api.get, `stock/${product.id}`);
+
+
+   if(avalableStockResponse.data.quantity > currentQuantity) {
+      console.log('deu certo');
+   }else{
+      console.log('falta estoque');
+   }
 }
 
 
 export default all([
-   takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
+   takeLatest('ADD_PRODUCT_TO_CART_REQUEST', checkProductStock)
 ]);
